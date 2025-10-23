796f28dcbb93b9d37d4cb2ec47d148cb5d2b44b9
mszabatin1 <mszabatin1@st.swps.edu.pl>
2025-10-23 23:03:06 +0200

    dodano kod models, udane założenie bazy danych oraz działająca aplikacja przez adres

--- moj_projekt/settings.py
+++ moj_projekt/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'workout_journal',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',     
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
