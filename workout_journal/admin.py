13cacdc8c7d63ebf63673ed4a572bb5c124f5baa
mszabatin1 <mszabatin1@st.swps.edu.pl>
2026-01-13 14:23:19 +0100

    lab_04

--- workout_journal/admin.py
+++ workout_journal/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Genre, Author, Book
+from .models import Genre, Author, Book, Osoba, Stanowisko
+
+class OsobaAdmin(admin.ModelAdmin):
+    list_display = ['imie', 'nazwisko', 'stanowisko']
+    list_filter = ["stanowisko", "data_dodania"]
 
 admin.site.register(Genre)
 admin.site.register(Author)
 admin.site.register(Book)
+admin.site.register(Osoba, OsobaAdmin)
