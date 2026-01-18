28bc01480a98b68b3ee6297d5cfdaec7bb61e1af
mszabatin1 <mszabatin1@st.swps.edu.pl>
2026-01-18 15:46:10 +0100

    lab_06

--- workout_journal/admin.py
+++ workout_journal/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import Genre, Author, Book, Osoba, Stanowisko
 
+
+
 class OsobaAdmin(admin.ModelAdmin):
     list_display = ['imie', 'nazwisko', 'stanowisko']
     list_filter = ["stanowisko", "data_dodania"]
---
+++ workout_journal/serializers.py
@@ -0,0 +1,117 @@
+from rest_framework import serializers
+from .models import Book, Author, Genre, MONTHS, BOOK_FORMATS, Osoba, Stanowisko
+from rest_framework.validators import UniqueTogetherValidator
+# class BookSerializer (serializers.Serializer):
+#     id = serializers.IntegerField(read_only = True)
+
+#     title = serializers.CharField(required = True)
+
+#     publication_month = serializers.ChoiceField(choices = MONTHS.choices, default = MONTHS.choices[0][0])
+
+#     book_format = serializers.ChoiceField(choices = BOOK_FORMATS, default = BOOK_FORMATS[0][0])
+
+#     author = serializers.PrimaryKeyRelatedField(queryset = Author.objects.all(), allow_null = True)
+
+#     genere = serializers.PrimaryKeyRelatedField(queryset = Genre.objects.all(), allow_null = True)
+
+#     available_copies = serializers.IntegerField(default = 1)
+
+#     def create(self, validated_data):
+#         return Book.objects.create(**validated_data)
+    
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.publication_month = validated_data.get('publication_month', instance.publication_month)
+#         instance.book_format = validated_data.get('book_format', instance.book_format)
+#         instance.author = validated_data.get('author', instance.author)
+#         instance.genere = validated_data.get('genere', instance.genere)
+#         instance.available_copies = validated_data.get('available_copies', instance.available_copies)
+#         instance.save()
+#         return instance
+class BookSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Book
+
+        field = ['id', 'title', 'publication_month', 'book_format', 'author', 'genre', 'available_copies']
+
+        read_only_fields = ['id']
+        
+        def validate_title(self, value):
+            if not value.istitle():
+                raise serializers.ValidationError("Tytuł książki powinien rozpoczynać się wielką literą!")
+            return value
+        
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+        
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Author.objects.all(),
+                fields=['first_name', 'last_name']
+            )
+        ]
+
+    def validate(self, data):
+        """
+        Walidacja całego obiektu autora.
+        Sprawdza poprawność formatu imienia, nazwiska i kodu kraju.
+        """
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        country = data.get('country')
+
+        # Imię i nazwisko powinny zaczynać się wielką literą
+        if first_name and not first_name.istitle():
+            raise serializers.ValidationError(
+                {"first_name": "Imię powinno rozpoczynać się wielką literą!"}
+            )
+
+        if last_name and not last_name.istitle():
+            raise serializers.ValidationError(
+                {"last_name": "Nazwisko powinno rozpoczynać się wielką literą!"}
+            )
+
+        # Kod kraju: dokładnie 2 wielkie litery
+        if country and (len(country) != 2 or not country.isupper()):
+            raise serializers.ValidationError(
+                {"country": "Kod kraju musi składać się z 2 wielkich liter, np. 'PL'."}
+            )
+
+        return data
+    
+def multiple_of_two(value):
+    if value % 2 != 0:
+        raise serializers.ValidationError("Ocena popularności musi być wielokrotnością 2 (np. 0, 2, 4, 6, 8, 10).")
+    
+class GenreSerializer(serializers.ModelSerializer):
+    popularity_rank = serializers.IntegerField(validators=[multiple_of_two])
+    
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+class OsobaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Osoba
+        fields = "__all__"
+        
+    def validate_imie(self, value):
+        if not value.istitle():
+            raise serializers.ValidationError(
+                "Imię powininno zawierać tylko litery i rozpoczynać się wielką literą!"
+            )
+        return value
+    
+    def validate_nazwisko(self, value):
+        if not value.istitle():
+            raise serializers.ValidationError(
+                "Nazwisko powininno zawierać tylko litery i rozpoczynać się wielką literą!"
+            )
+        return value
+    
+class StanowiskoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stanowisko
+        fields = "__all__"
